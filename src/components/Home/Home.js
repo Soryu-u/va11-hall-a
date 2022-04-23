4c51b549f023e4451c226f74e68267bca63262b1
Soryu <collages447@gmail.com>
2022-04-23 22:35:05 +0300

    feat: add routes and pages

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Home.module.css"
+
+export default function Home(){
+    return(
+        <div className={style.container}>
+            <h1>Home page</h1>
+        </div>
+    )
+}
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from  "./Sidebar.module.css"
+
+export default function Sidebar () {
+    return(
+        <div className={style.sidebar}>
+            Sidebar
+        </div>
+    )
+}
